a96d1d09e69a6973cdef7c4349f6a485e54fe458
Gonza <goialarcon@gmail.com>
2023-01-07 20:53:13 -0300

    initial commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,80 @@
+const urlApi = 'https://mindicador.cl/api/';
+const filtrarMonedas = ["dolar", "euro", "uf", "utm"];
+const selectWithCurrencies = document.querySelector("#moneda");
+const divResult = document.querySelector("#result");
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+const calcResult = (amount) => `$${(amount/selectWithCurrencies.value).toFixed(2)}`;
+
+// obteniendo monedas desde la API
+const getCurrencies = async () =>{
+    const reqCurrencies = await fetch(urlApi);
+    const resData = await reqCurrencies.json();
+
+    // obteniendo el código de las divisas
+    const currencyList = filtrarMonedas.map((currency)=>{
+        return{
+            code: resData[currency].codigo,
+            value: resData[currency].valor,
+        }
+    } )
+
+    //mostrando monedas
+    currencyList.forEach((localCurrency)=> {
+        const selectOption = document.createElement("option");
+        selectOption.value = localCurrency.value;
+        selectOption.text = capitalize(localCurrency.code);
+        selectWithCurrencies.appendChild(selectOption);
+    })   
+}
+ 
+//gráfico
+const drawChart = async () => {
+    try{
+        const currency =
+        selectWithCurrencies.options[
+            selectWithCurrencies.selectedIndex
+        ].text.toLowerCase();
+
+
+        const reqChart = await fetch(`${urlApi}/${currency}`);
+        const dataChart = await reqChart.json();
+
+        const serietoChart = dataChart.serie.slice(0, 10).reverse();
+
+        const data = {
+            labels: serietoChart.map((item)=> item.fecha.substring(0, 10)),
+            datasets: [
+                {
+                    label: currency,
+                    data: serietoChart.map((item) =>item.valor),
+                },
+            ],
+        };
+        const config = {
+            type: "line",
+            data: data,
+        };
+
+        const chartDom =document.querySelector("#chart");
+        chartDom.classList.remove("d-none");
+        new Chart(chartDom, config);
+
+    } catch (error){
+        alert('Hubo un problema al obtener los datos para generar el gráfico');
+        console.log(error);
+    }
+
+}
+
+document.querySelector("#btnConvert").addEventListener('click', ()=>{
+    const amountPesos = document.querySelector('#pesos').value;
+    if(amountPesos == ''){
+        alert('Debes ingresar un valor');
+        return;
+    }
+    divResult.innerHTML=calcResult(amountPesos);
+    drawChart();
+});
+
+getCurrencies();
